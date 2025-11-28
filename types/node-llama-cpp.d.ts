cdb7f4bdfa9a3802a70497069c0b4c6cd2f3240d
Plataforma de estudos UPGRADE – Next.js + Supabase + Tailwind <fmedeiros58@gmail.com>
2025-11-28 16:58:51 -0300

    chore: add node-llama-cpp types

---
+++ types/node-llama-cpp.d.ts
@@ -0,0 +1,9 @@
+declare module "node-llama-cpp" {
+  export const Llama: any;
+  export const LlamaModel: any;
+  export const LlamaContext: any;
+  export const LlamaChatSession: any;
+  export type NodeLlamaModule = any;
+  const value: any;
+  export default value;
+}
